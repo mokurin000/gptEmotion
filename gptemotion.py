591fb5f16f561784d05ccb226cb1b8e58a60df95
poly000 <1348292515a@gmail.com>
2024-12-05 02:25:43 +0800

    fix: switch to deadlock-free cache lib

--- gptemotion.py
+++ gptemotion.py
@@ -4,7 +4,7 @@ from os import listdir, path
 
 import polars as pl
 from openai import OpenAI
-from cachier import cachier
+from diskcache import Cache
 from dotenv import load_dotenv
 from pydantic import BaseModel
 
@@ -27,8 +27,9 @@ OUTPUT_PATH = os.environ["OUTPUT_PATH"]
 
 OPENAI = OpenAI()
 
+cache = Cache("temp")
 
-@cachier()
+
 def process_with_gpt(text: str) -> Result | None:
     context = [
         {"role": "system", "content": "输出必须在五字以内，不包含标点符号"},
@@ -45,20 +46,29 @@ def process_with_gpt(text: str) -> Result | None:
             n=1,
             temperature=0.2,
             response_format=Result,
+            timeout=10.0,
         )
         content = completion.choices[0].message.parsed
         return content
     except KeyboardInterrupt:
         exit(1)
     except Exception as e:
-        print(f"failed to process {text}:", e)
+        logger.error(f"failed to process {text}:", e)
         return None
 
 
 def process(text: str) -> dict | Result:
     DEFAULT = {"情感体验": None, "事件认知": None, "行为反应": None}
 
-    result = process_with_gpt(text)
+    if text is None:
+        return DEFAULT
+
+    logger.info(f"started process for text {text.__repr__()}")
+    if text in cache:
+        result = cache[text]
+    else:
+        result = process_with_gpt(text)
+        cache[text] = result
     logger.info(f"{text.__repr__()}: {result}")
     return result.model_dump() if result is not None else DEFAULT
 
@@ -73,14 +83,12 @@ def main():
         output_path = path.join(OUTPUT_PATH, filename)
         df = pl.read_excel(input_path)
 
-        df = df.limit(20)
-        df = df.with_columns(
+        df.with_columns(
             pl.col("评论内容")
             .map_elements(process, return_dtype=pl.Struct, strategy="threading")
             .alias("result")
         ).unnest("result")
         df.write_excel(output_path)
-        break
 
 
 if __name__ == "__main__":
